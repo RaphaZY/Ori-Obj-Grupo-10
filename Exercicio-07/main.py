a3bd065ed658c760780d54a10053535cd4f2fd39
Raphael Venancio Coelho <raphacoelho3790@gmail.com>
2025-10-21 14:30:55 -0300

    refactor: Criação da função main()

--- Exercicio-07/main.py
+++ Exercicio-07/main.py
@@ -1,15 +1,18 @@
 from model.funcionario import Funcionario
 
+def main():
+    funcionario1 = Funcionario()
+    funcionario1.set_nome("Gabriel M.")
+    funcionario1.set_salario_bruto(1800)
+    funcionario1.set_total_acrescimos(100)
+    funcionario1.set_total_descontos(50)
 
-funcionario1 = Funcionario()
-funcionario1.set_nome("Gabriel M.")
-funcionario1.set_salario_bruto(1800)
-funcionario1.set_total_acrescimos(100)
-funcionario1.set_total_descontos(50)
+    print(f"Nome: {funcionario1.get_nome()}")
+    print(f"Salário Bruto: {funcionario1.get_salario_bruto()}")
+    print(f"Salário Liquido: {funcionario1.calcular_salario_liquido()}")
 
 
 if __name__ == '__main__':
     
-    print(f"Nome: {funcionario1.get_nome()}")
-    print(f"Salário Bruto: {funcionario1.get_salario_bruto()}")
-    print(f"Salário Liquido: {funcionario1.calcular_salario_liquido()}")
+    main()
+  
