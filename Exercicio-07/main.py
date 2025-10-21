28f6e3e1ff1af893dd78fdce57846da82fc4a176
Raphael Venancio Coelho <raphacoelho3790@gmail.com>
2025-10-21 11:33:28 -0300

    refactor: Separação da classe funcionario e organização da main.py

--- Exercicio-07/main.py
+++ Exercicio-07/main.py
@@ -1,35 +1,15 @@
-class Funcionario:
+from model.funcionario import Funcionario
+
+
+funcionario1 = Funcionario()
+funcionario1.set_nome("Gabriel M.")
+funcionario1.set_salario_bruto(1800)
+funcionario1.set_total_acrescimos(100)
+funcionario1.set_total_descontos(50)
+
+
+if __name__ == '__main__':
     
-    def __init__(self, nome, salario_bruto, total_acrescimos=0, total_descontos=0):
-        self.__nome = nome
-        self.__salario_bruto = salario_bruto
-        self.__total_acrescimos = total_acrescimos
-        self.__total_descontos = total_descontos
-    
-    def get_nome(self):
-        return self.__nome
-    
-    def get_salario_bruto(self):
-        return self.__salario_bruto
-    
-    def get_total_acrescimos(self):
-        return self.__total_acrescimos
-    
-    def get_total_descontos(self):
-        return self.__total_descontos
-    
-    def set_nome(self, nome):
-        self.__nome = nome
-    
-    def set_salario_bruto(self, salario_bruto):
-        self.__salario_bruto = salario_bruto
-    
-    def set_total_acrescimos(self, total_acrescimos):
-        self.__total_acrescimos = total_acrescimos
-    
-    def set_total_descontos(self, total_descontos):
-        self.__total_descontos = total_descontos
-    
-    def calcular_salario_liquido(self):
-        salario_liquido = self.__salario_bruto + self.__total_acrescimos - self.__total_descontos
-        return salario_liquido
+    print(f"Nome: {funcionario1.get_nome()}")
+    print(f"Salário Bruto: {funcionario1.get_salario_bruto()}")
+    print(f"Salário Liquido: {funcionario1.calcular_salario_liquido()}")
